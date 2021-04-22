bfc2c223fbdd46b92f1219eee4b9d96db116fb91
Rob Weber <robweberjr@gmail.com>
2021-04-22 11:08:14 -0500

    syntax fix

--- src/omni_epd/virtualepd.py
+++ src/omni_epd/virtualepd.py
@@ -98,7 +98,7 @@ class VirtualEPD:
                 palette_image = Image.new("P", (1, 1))
 
                 # set the palette, set all other colors to 0
-                palette_image.putpalette(palette + [0,0,0] * (256-len(palette)))
+                palette_image.putpalette(palette + [0, 0, 0] * (256-len(palette)))
 
                 # load image so it can be processed
                 image.load()
