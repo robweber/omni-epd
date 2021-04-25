28b5aea43c12b204d49ff8d5481b3bfde03b35d9
Rob Weber <robweberjr@gmail.com>
2021-04-25 21:42:45 +0100

    added additional tests for mode and palette filtering

--- tests/test_omni_epd.py
+++ tests/test_omni_epd.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 import time
-from omni_epd import EPDNotFoundError
+from omni_epd import EPDNotFoundError, EPDConfigurationError
 from omni_epd import displayfactory
 from omni_epd.virtualepd import VirtualEPD
 from omni_epd.conf import IMAGE_DISPLAY, CONFIG_FILE
@@ -10,6 +10,7 @@ from omni_epd.conf import IMAGE_DISPLAY, CONFIG_FILE
 class TestomniEpd(unittest.TestCase):
     goodEpd = "omni_epd.mock"  # this should always be a valid EPD
     badEpd = "omni_epd.bad"  # this is not a valid EPD
+    badConfig = 'bad_conf.ini'  # name of invalid configuration file
 
     def test_supported_diplays(self):
         drivers = displayfactory.list_supported_displays()
@@ -34,6 +35,9 @@ class TestomniEpd(unittest.TestCase):
         assert epd._config.has_option(IMAGE_DISPLAY, 'rotate')
         assert epd._config.getfloat(IMAGE_DISPLAY, 'rotate') == 90
 
+        # test that mode is default
+        assert epd.mode == 'bw'
+
         # reset global config file, wait for file IO
         os.rename(os.path.join(os.getcwd(), CONFIG_FILE), os.path.join(os.getcwd(), "tests", CONFIG_FILE))
         time.sleep(1)
@@ -51,6 +55,10 @@ class TestomniEpd(unittest.TestCase):
         # device should override global
         assert epd._config.has_option(IMAGE_DISPLAY, 'flip_horizontal')
         self.assertFalse(epd._config.getboolean(IMAGE_DISPLAY, 'flip_horizontal'))
+
+        # test mode and palette configurations
+        assert epd.mode == 'palette'
+        assert len(epd.palette_filter) == 5
 
         # reset global config file, wait for file IO
         os.rename(os.path.join(os.getcwd(), CONFIG_FILE), os.path.join(os.getcwd(), "tests", CONFIG_FILE))
@@ -78,4 +86,16 @@ class TestomniEpd(unittest.TestCase):
         # reset global config file, wait for file IO
         os.rename(os.path.join(os.getcwd(), CONFIG_FILE), os.path.join(os.getcwd(), "tests", CONFIG_FILE))
         os.rename(os.path.join(os.getcwd(), deviceConfig), os.path.join(os.getcwd(), "tests", deviceConfig))
+        time.sleep(1)
+
+    def test_configuration_error(self):
+        deviceConfig = self.goodEpd + ".ini"
+
+        # copy bad config file to be loaded
+        os.rename(os.path.join(os.getcwd(), "tests", self.badConfig), os.path.join(os.getcwd(), deviceConfig))
+
+        # load the display driver, shoudl throw EPDConfigurationError
+        self.assertRaises(EPDConfigurationError, displayfactory.load_display_driver, self.goodEpd)
+
+        os.rename(os.path.join(os.getcwd(), deviceConfig), os.path.join(os.getcwd(), "tests", self.badConfig))
         time.sleep(1)
