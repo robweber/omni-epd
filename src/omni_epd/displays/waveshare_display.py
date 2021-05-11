4e70cd8f595a810f0992f289e468559106736c0e
Rob Weber <robweberjr@gmail.com>
2021-05-11 09:20:51 -0500

    added epd1in54_V2 clear() fix

--- src/omni_epd/displays/waveshare_display.py
+++ src/omni_epd/displays/waveshare_display.py
@@ -82,7 +82,7 @@ class WaveshareBWDisplay(WaveshareDisplay):
 
     # devices that use alternate init methods
     deviceMap = {"epd1in54": {"alt_init": True, "lut_init": True, "alt_clear": True},
-                 "epd1in54_V2": {"alt_init": False, "lut_init": False, "alt_clear": False},
+                 "epd1in54_V2": {"alt_init": False, "lut_init": False, "alt_clear": True},
                  "epd2in9": {"alt_init": True, "lut_init": True, "alt_clear": True},
                  "epd2in9_V2": {"alt_init": False, "lut_init": False, "alt_clear": False},
                  "epd2in9d": {"alt_init": False, "lut_init": False, "alt_clear": False},
