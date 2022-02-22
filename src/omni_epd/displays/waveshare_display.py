3229393c11d8cd2c6855892976cd3ed14c06c825
Rob Weber <robweberjr@gmail.com>
2022-02-22 09:09:04 -0600

    revert this one line to original

--- src/omni_epd/displays/waveshare_display.py
+++ src/omni_epd/displays/waveshare_display.py
@@ -214,7 +214,7 @@ class WaveshareTriColorDisplay(WaveshareDisplay):
             img_black.putpalette((0, 0, 0, 255, 255, 255))
 
             img_color = image.copy()
-            img_color.putpalette((255, 255, 255, 255, 128, 0, 0, 0, 0))
+            img_color.putpalette((255, 255, 255, 255, 255, 255, 0, 0, 0) + (255, 255, 255)*253)
 
             self._device.display(self._device.getbuffer(img_black), self._device.getbuffer(img_color))
 
