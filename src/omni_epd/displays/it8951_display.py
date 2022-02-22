138e8d90cce57351d99b0b16c2b32dcfd0196f62
Rob Weber <robweberjr@gmail.com>
2022-02-22 10:30:42 -0600

    added it communications based on example code in repo

--- src/omni_epd/displays/it8951_display.py
+++ src/omni_epd/displays/it8951_display.py
@@ -24,24 +24,30 @@ from .. virtualepd import VirtualEPD
 
 class IT8951Display(VirtualEPD):
     """
-    This is a reference implementation of a display extending VirtualEPD
-    it can write images to a testing file for use as a mock testing device
+    This will communicate with IT8951 type displays utilzing the driver built by Greg GregDMeyer
+    https://github.com/GregDMeyer/IT8951
     """
 
-    pkg_name = 'waveshare_epd'
-    max_colors = 256
-    modes_available = ('bw', 'color', 'palette')
+    pkg_name = 'IT8951'
+    it8951_constants = None
 
     def __init__(self, deviceName, config):
         super().__init__(deviceName, config)
 
         self.logger = logging.getLogger(__name__)
 
-        # this is normally where you'd load actual device class
+        # load the IT8951.display package and create the object
+        deviceObj = self.load_display_driver(self.pkg_name, "display")
+        self._device = deviceObj.AutoEPDDisplay(vcom=self._getfloat_device_option('vcom', -2.06),
+                                                spi_hz=self._getint_device_option('spi_hz', 24000000),
+                                                rotate=self._get_device_option('rotate', None))
 
         # set the width and height
-        self.width = 400
-        self.height = 200
+        self.width = self._device.width
+        self.height = self._device.height
+
+        # load the from IT8951.constants
+        self.it8951_constants = self.load_display_driver(self.pkg_name, "constants")
 
     @staticmethod
     def get_supported_devices():
@@ -50,18 +56,33 @@ class IT8951Display(VirtualEPD):
 
     def prepare(self):
         self.logger.info(f"preparing {self.__str__()}")
+        self._device.epd.run()
 
     def _display(self, image):
         # keep this as it applies any filters defined in INI file
         image = self._filterImage(image)
 
+        self.clear()  # not sure if this is needed, was part of example
+
+        dims = (self.width, self.height)
+        image.thumbnail(dims)
+
+        paste_coords = [dims[i] - image.size[i] for i in (0,1)]  # align image with bottom of display
+
+        # write image to display
+        self._device.frame_buf.paste(pil_im, paste_coords)
+        self._device.draw_full(it8951_constants.DisplayModes.GC16)
+
         self.logger.info(f"{self.__str__()} writing image to EPD")
 
     def sleep(self):
         self.logger.info(f"{self.__str__()} is sleeping")
+        self._device.epd.sleep()
 
     def clear(self):
         self.logger.info(f"clearing {self.__str__()}")
+        self._device.clear()
 
     def close(self):
         self.logger.info(f"closing {self.__str__()}")
+        # doesn't appear to be a close method for this EPD
