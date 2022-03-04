12f01b3f7163ea9b661873b394606fcbc2299c58
Rob Weber <robweberjr@gmail.com>
2022-03-04 13:28:43 -0600

    Image.point returns an Image, thanks missionfloyd

--- src/omni_epd/displays/waveshare_display.py
+++ src/omni_epd/displays/waveshare_display.py
@@ -212,17 +212,12 @@ class WaveshareTriColorDisplay(WaveshareDisplay):
             # convert to greyscale
             image = image.convert('L')
 
-            # separate out black from the image
-            img_black = image.copy()
-
             # convert greys to black or white based on threshold of 20
-            img_black = img_black.point(lambda p: 255 if p >= 20 else 0)
-
-            # separate out third color from the image
-            img_color = image.copy()
+            # https://pillow.readthedocs.io/en/stable/reference/Image.html#PIL.Image.Image.point
+            img_black = image.point(lambda p: 255 if p >= 20 else 0)
 
             # convert greys to third color (represented as black in the image) or white based on threshold of 20
-            img_color = img_color.point(lambda p: 0 if 20 < p < 235 else 255)
+            img_color = image.point(lambda p: 0 if 20 < p < 235 else 255)
 
             # send to display
             self._device.display(self._device.getbuffer(img_black), self._device.getbuffer(img_color))
